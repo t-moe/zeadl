2efaab8dd959ad60e9f58ee09362e761da39805a
Adrian Maag <admaag@gmx.ch>
2015-04-13 20:51:41 +0200

    Table with save function added Time format, channel names e.g. SeekBar with EditText for Samplerate added

---
+++ app/src/main/java/ch/bfh/android/zeadl/activity/FileClass.java
@@ -0,0 +1,27 @@
+package ch.bfh.android.zeadl.activity;
+
+import android.os.Environment;
+
+/**
+ * Created by adrian on 13.04.15.
+ */
+public class FileClass {
+
+    public boolean checkExternalStorageWritable(){
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+}
