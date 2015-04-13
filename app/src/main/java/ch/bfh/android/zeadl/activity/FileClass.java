b178d6386e5d32d4cf8a5a7da2ebec9fea1a3d37
Adrian Maag <admaag@gmx.ch>
2015-04-13 22:01:12 +0200

    Samplerate Setting added Table and Save finished Design modified at Settings View

--- app/src/main/java/ch/bfh/android/zeadl/activity/FileClass.java
+++ app/src/main/java/ch/bfh/android/zeadl/activity/FileClass.java
@@ -1,11 +1,28 @@
 package ch.bfh.android.zeadl.activity;
 
+import android.content.Context;
 import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOError;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import ch.bfh.android.zeadl.sensor.SensorGroup;
+
+import static android.app.PendingIntent.getActivity;
 
 /**
  * Created by adrian on 13.04.15.
  */
 public class FileClass {
+
+    //public FileClass(){}
+
 
     public boolean checkExternalStorageWritable(){
         String state = Environment.getExternalStorageState();
@@ -23,5 +40,47 @@ public class FileClass {
         }
         return false;
     }
+
+    public File getStorageDir(String filename) {
+        File file = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS),
+                filename);
+        if (!file.mkdirs()) {
+            Log.e("TableSave", "Directory not created");
+        }
+        return file;
+    }
+
+
+    public void saveSegment(SensorGroup.DataSegment segment) throws IOException
+    {
+
+        if(checkExternalStorageWritable()) {
+
+            BufferedWriter bfw = new BufferedWriter(new FileWriter("/sdcard/DCIM/Data.txt"));
+            SimpleDateFormat df = new SimpleDateFormat("yyMMddHHmmss.S");
+
+            bfw.write("Time, ");
+            for (int i = 0; i < segment.getChannels().size(); i++) {
+                bfw.write(segment.getChannels().get(i).getName());
+                bfw.write(", ");
+            }
+
+            for (int i = 0; i < segment.getEntries().size(); i++) {
+                bfw.newLine();
+                bfw.write(df.format(segment.getEntries().get(i).getTime()));
+                for (int j = 0; j < segment.getChannels().size(); j++) {
+                    bfw.write(segment.getEntries().get(i).getChannelData().get(j) + ", ");
+                }
+            }
+
+            bfw.close();
+
+            //Toast.makeText(getActivity(),"Data saved",Toast.LENGTH_SHORT).show();
+        }
+        //Toast.makeText(getActivity(),"Data not saved, Error",Toast.LENGTH_LONG).show();
+
+    }
+
 
 }
--- app/src/main/java/ch/bfh/android/zeadl/activity/TableClass.java
+++ app/src/main/java/ch/bfh/android/zeadl/activity/TableClass.java
@@ -4,6 +4,15 @@ import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import ch.bfh.android.zeadl.sensor.SensorGroup;
 
 /**
  * Created by adrian on 11.04.15.
@@ -12,23 +21,63 @@ public class TableClass {
 
     private Activity activity;
     private TableLayout table_layout;
-    public TableClass(TableLayout table_layout){
+    private SensorGroup group;
+
+    TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+
+    public TableClass(TableLayout table_layout, Activity activity, SensorGroup group){
         this.table_layout = table_layout;
-
+        this.activity = activity;
+        this.group = group;
 
     }
 
-    public void addRow(TableRow row){
-        table_layout.addView(row);
-    }
-
-    public TableRow generateRow(int time, int data1, int data2){
+    public void addSegment(SensorGroup.DataSegment segment){
         TableRow row = new TableRow(activity);
+        TextView tv;
+        Date date;
+        SimpleDateFormat df = new SimpleDateFormat("yyMMddHHmmss.S");
 
+        row.setLayoutParams(params);
+        row.setPadding(5,1,5,1);
 
+        tv = new TextView(activity);
+        tv.setText("Time   ");
+        tv.setBackgroundColor(0x88888888);
+        row.addView(tv);
 
-        return row;
+        for(int i=0;i<segment.getChannels().size();i++){
+            tv = new TextView(activity);
+            tv.setPadding(0,0,10,0);
+            tv.setBackgroundColor(0xBBBBBBBB);
+            tv.setText(segment.getChannels().get(i).getName());
+
+            row.addView(tv);
+        }
+
+        table_layout.addView(row);
+
+        for(int i=0;i<segment.getEntries().size();i++){
+            row = new TableRow(activity);
+
+            tv = new TextView(activity);
+            tv.setPadding(0,0,5,0);
+            //date = segment.getEntries().get(i).getTime();
+            //tv.setText(""+date.getTime());
+
+            tv.setText(df.format(segment.getEntries().get(i).getTime()));
+
+            row.addView(tv);
+
+            for (int j=0;j<segment.getChannels().size();j++){
+                tv = new TextView(activity);
+                tv.setPadding(0,0,5,0);
+                tv.setText(""+segment.getEntries().get(i).getChannelData().get(j));
+
+                row.addView(tv);
+            }
+
+            table_layout.addView(row);
+        }
     }
-
-
 }
